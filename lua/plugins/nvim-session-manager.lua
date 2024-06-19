c17d1b2fbfb591741d712023e58f9faf44a0441e
fan.hu <fan.hu@sienergys.com>
2024-06-19 21:38:38 +0800

    commenter unused config

--- lua/plugins/nvim-session-manager.lua
+++ lua/plugins/nvim-session-manager.lua
@@ -3,8 +3,8 @@ local config = require('session_manager.config')
 
 require('session_manager').setup({
   sessions_dir = Path:new(vim.fn.stdpath('data'), 'sessions'), -- The directory where the session files will be saved.
-  session_filename_to_dir = session_filename_to_dir,           -- Function that replaces symbols into separators and colons to transform filename into a session directory.
-  dir_to_session_filename = dir_to_session_filename,           -- Function that replaces separators and colons into special symbols to transform session directory into a filename. Should use `vim.uv.cwd()` if the passed `dir` is `nil`.
+  -- session_filename_to_dir = session_filename_to_dir,           -- Function that replaces symbols into separators and colons to transform filename into a session directory.
+  -- dir_to_session_filename = dir_to_session_filename,           -- Function that replaces separators and colons into special symbols to transform session directory into a filename. Should use `vim.uv.cwd()` if the passed `dir` is `nil`.
   autoload_mode = config.AutoloadMode.CurrentDir,              -- Define what to do when Neovim is started without arguments. See "Autoload mode" section below.
   autosave_last_session = true,                                -- Automatically save last session on exit and on session switch.
   autosave_ignore_not_normal = true,                           -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
