c2bb86c2e39635a4729f0dec3d3978c7939ebf8e
fan.hu <fan.hu@sienergys.com>
2024-06-21 16:04:56 +0800

    add telescope key maps

--- lua/geek/plugins/telescope.lua
+++ lua/geek/plugins/telescope.lua
@@ -62,5 +62,8 @@ return {
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+
+    local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
+    keymap.set('n', "<leader>a", live_grep_args_shortcuts.grep_word_under_cursor)
   end
 }
