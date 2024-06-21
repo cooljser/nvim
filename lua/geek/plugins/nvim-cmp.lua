3e3d9c002a17f279386850c2b4012d39d92c820f
fan.hu <fan.hu@sienergys.com>
2024-06-21 17:32:16 +0800

    update nvim-cmp load event

--- lua/geek/plugins/nvim-cmp.lua
+++ lua/geek/plugins/nvim-cmp.lua
@@ -1,11 +1,12 @@
 return {
   'hrsh7th/nvim-cmp',
-  event = 'InsertEnter',
+  event = { 'BufReadPre', 'BufNewFile' },
   dependencies = {
     'hrsh7th/cmp-nvim-lsp', -- lsp auto-completion
     'hrsh7th/cmp-buffer', -- source for text in buffer
     'hrsh7th/cmp-path', -- source for file system paths
     'hrsh7th/cmp-cmdline', -- cmdline auto-completion
+    'saadparwaiz1/cmp_luasnip', -- for autocompletion
     {
       'L3MON4D3/LuaSnip',
       -- follow latest release.
@@ -13,7 +14,6 @@ return {
       -- install jsregexp (optional!).
       build = 'make install_jsregexp',
     },
-    'saadparwaiz1/cmp_luasnip', -- for autocompletion
     'rafamadriz/friendly-snippets', -- useful snippets
     'onsails/lspkind.nvim', -- vs-code like pictograms
   },
