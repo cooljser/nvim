e2905c9252a17fdc7c1e19d3e45ebe7fc2e52f07
fan.hu <fan.hu@sienergys.com>
2024-06-22 21:39:38 +0800

    clear codeium tips when exit normal mode, remove linting config

--- lua/geek/plugins/ai.lua
+++ lua/geek/plugins/ai.lua
@@ -1,9 +1,14 @@
 return {
   'Exafunction/codeium.vim',
   enabled = true,
-  tag = "1.8.37",
+  tag = '1.8.37',
   event = 'BufEnter',
   config = function()
     vim.g.codeium_idle_delay = 100
-  end
+
+    -- set keymaps
+    local keymap = vim.keymap
+
+    keymap.set('i', '<C-c>', '<ESC>:call codeium#Clear()<CR>', { noremap = true, silent = true })
+  end,
 }
