f5dd00f117248a06ab48c4546cf5446922316e90
fan.hu <fan.hu@sienergys.com>
2024-07-01 18:15:52 +0800

    add nvim-cmp cmd-line setup

--- lua/geek/plugins/nvim-cmp.lua
+++ lua/geek/plugins/nvim-cmp.lua
@@ -117,5 +117,20 @@ return {
         }),
       },
     })
+
+    -- `:` cmdline setup.
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' },
+      }, {
+        {
+          name = 'cmdline',
+          option = {
+            ignore_cmds = { 'Man', '!' },
+          },
+        },
+      }),
+    })
   end,
 }
