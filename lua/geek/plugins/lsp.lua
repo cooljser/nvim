ff6b51aed68cbabf78591d994e95739fc31fcc77
fan.hu <fan.hu@sienergys.com>
2025-10-13 18:07:02 +0800

    update config

--- lua/geek/plugins/lsp.lua
+++ lua/geek/plugins/lsp.lua
@@ -19,27 +19,7 @@ return {
 
     require('lspsaga').setup({})
 
-    require('mason-lspconfig').setup({
-      -- A list of servers to automatically install if they're not already installed
-      ensure_installed = {
-        'pylsp',
-        'lua_ls',
-        'rust_analyzer',
-        'tsserver',
-        'cssmodules_ls',
-        'emmet_language_server',
-        'jdtls',
-      },
-    })
-
-    -- Set different settings for different languages' LSP
-    -- LSP list: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    -- How to use setup({}): https://github.com/neovim/nvim-lspconfig/wiki/Understanding-setup-%7B%7D
-    --     - the settings table is sent to the LSP
-    --     - on_attach: a lua callback function to run after LSP atteches to a given buffer
-    local lspconfig = require('lspconfig')
-
-    -- Customized on_attach function
+    -- Diagnostic 键映射设置
     -- See `:help vim.diagnostic.*` for documentation on any of the below functions
     local opts = { noremap = true, silent = true }
     vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
@@ -47,11 +27,10 @@ return {
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
     vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
-    -- Use an on_attach function to only map the following keys
-    -- after the language server attaches to the current buffer
+    -- on_attach 函数：在 LSP 附加到缓冲区后映射键位
     local on_attach = function(_, bufnr)
       -- Enable completion triggered by <c-x><c-o>
-      vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+      vim.api.nvim_set_option_value('omnifunc', 'v:lua.vim.lsp.omnifunc', { buf = bufnr })
 
       -- See `:help vim.lsp.*` for documentation on any of the below functions
       local bufopts = { noremap = true, silent = true, buffer = bufnr }
@@ -59,65 +38,74 @@ return {
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
       vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<CR>', bufopts)
       vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-      -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-      -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-      -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-      -- vim.keymap.set('n', '<space>wl', function()
-      -- print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      -- end, bufopts)
-      -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
       vim.keymap.set('n', '<space>rn', '<cmd>Lspsaga rename<CR>', bufopts)
       vim.keymap.set('n', '<space>ca', '<cmd>Lspsaga code_action<CR>', bufopts)
       vim.keymap.set('n', 'gr', '<cmd>Lspsaga finder<CR>', bufopts)
-      -- vim.keymap.set("n", "<space>f", function()
-      -- vim.lsp.buf.format({ async = true })
-      -- end, bufopts)
     end
 
-    -- Configure each language
-    -- How to add LSP for a specific language?
-    -- 1. use `:Mason` to install corresponding LSP
-    -- 2. add configuration below
-
+    -- 获取默认的 capabilities
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-    lspconfig.pylsp.setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
-
-    lspconfig.tsserver.setup({
-      capabilities = capabilities,
-      filetypes = {
-        'javascript',
-        'javascriptreact',
-        'javascript.jsx',
-        'typescript',
-        'typescriptreact',
-        'typescript.tsx',
+    -- 特定语言服务器的配置
+    local server_configs = {
+      pylsp = {
+        capabilities = capabilities,
+        on_attach = on_attach,
       },
-      on_attach = on_attach,
-    })
+      tsserver = {
+        capabilities = capabilities,
+        filetypes = {
+          'javascript',
+          'javascriptreact',
+          'javascript.jsx',
+          'typescript',
+          'typescriptreact',
+          'typescript.tsx',
+        },
+        on_attach = on_attach,
+      },
+      lua_ls = {
+        capabilities = capabilities,
+        filetypes = { 'lua' },
+        on_attach = on_attach,
+      },
+      cssmodules_ls = {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      },
+      emmet_language_server = {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      },
+      jdtls = {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      },
+    }
 
-    lspconfig.lua_ls.setup({
-      capabilities = capabilities,
-      filetypes = { 'lua' },
-      on_attach = on_attach,
-    })
-
-    lspconfig.cssmodules_ls.setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
-
-    lspconfig.emmet_language_server.setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
-
-    lspconfig.jdtls.setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
+    -- 使用 mason-lspconfig 的 handlers 自动配置服务器
+    require('mason-lspconfig').setup({
+      -- 自动安装的服务器列表
+      ensure_installed = {
+        'pylsp',
+        'lua_ls',
+        'rust_analyzer',
+        'ts_ls',
+        'cssmodules_ls',
+        'emmet_language_server',
+        'jdtls',
+      },
+      -- 使用 handlers 自动配置服务器（推荐方式）
+      handlers = {
+        -- 默认 handler：为所有服务器应用默认配置
+        function(server_name)
+          local config = server_configs[server_name] or {
+            capabilities = capabilities,
+            on_attach = on_attach,
+          }
+          require('lspconfig')[server_name].setup(config)
+        end,
+      },
     })
   end,
 }
--- lua/geek/plugins/telescope.lua
+++ lua/geek/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
+  commit = 'b4da76be54691e854d3e0e02c36b0245f945c2c7',
+  -- tag = '0.1.8',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope-ui-select.nvim',
